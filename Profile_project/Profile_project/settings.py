179728d15e833ff8611035a47ab0616a27ab1b71
sandesh-kun <sandeshstone13@gmail.com>
2024-01-04 16:13:35 +0545

    Added Notification and PDFs

--- Profile_project/Profile_project/settings.py
+++ Profile_project/Profile_project/settings.py
@@ -152,9 +152,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sandeshstone13@gmail.com'
-EMAIL_HOST_PASSWORD = 'ttzvscvhmfgkysub'
-DEFAULT_FROM_EMAIL = 'sandeshstone13@gmail.com'
+EMAIL_HOST_USER = 'yourmail@gmail.com'
+EMAIL_HOST_PASSWORD = 'heheeehehehehe'
+DEFAULT_FROM_EMAIL = 'yourmail@gmail.com'
 
 LOGIN_URL = 'user_login'
 
